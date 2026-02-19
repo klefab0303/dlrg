769abff3e21f6ac54733847a9d3b7e7b598efd83
klefab0303 <klefab0303@gho365.de>
2026-02-19 17:25:11 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,8 +2,8 @@
 // Verbindet die App mit deiner Supabase-Datenbank.
 // ERSETZE die beiden Werte unten mit deinen eigenen Daten (Schritt-für-Schritt-Anleitung weiter unten).
 
-const SUPABASE_URL = 'https://DEINE-PROJEKT-URL.supabase.co';
-const SUPABASE_ANON_KEY = 'DEIN-ANON-PUBLIC-KEY';
+const SUPABASE_URL = 'https://Wettkampfschwimmen.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0Z250a3Rudm5heWtza3NxbHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MTYzNzEsImV4cCI6MjA4NzA5MjM3MX0.nSstbmb5FMQor-5WJoFjxfWRrVsWtgp-LrkNmr3wNiM';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
